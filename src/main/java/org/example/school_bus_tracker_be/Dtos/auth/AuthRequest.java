051b687508e058dc622354c31a362a722b647e0a
UMURERWLISAORNELLA <umurerwalisaornella@gmail.com>
2025-12-06 21:15:56 +0200

    project structure updated

---
+++ src/main/java/org/example/school_bus_tracker_be/Dtos/auth/AuthRequest.java
@@ -0,0 +1,4 @@
+package org.example.school_bus_tracker_be.Dtos.auth;
+
+public class AuthRequest {
+}
---
+++ src/main/java/org/example/school_bus_tracker_be/util/RoleValidator.java
@@ -0,0 +1,4 @@
+package org.example.school_bus_tracker_be.util;
+
+public class RoleValidator {
+}
